cb5148482169eb22b86e90505c1174accbb7e532
kleber.silva <kleber.silva@iteris.com.br>
2020-08-20 00:45:19 -0300

    authentication use case

---
+++ lib/domain/usecases/authentication.dart
@@ -0,0 +1,7 @@
+import 'package:meta/meta.dart';
+
+import '../entities/entities.dart';
+
+abstract class IAuthentication {
+  Future<AccountEntity> auth({@required String email, @required String password});
+}
