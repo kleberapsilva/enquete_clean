37337c06e74a450cc59f0442f12c5367f75c2914
kleber.silva <kleber.silva@iteris.com.br>
2020-08-20 01:29:50 -0300

    teste valores url e metodo

--- test/data/usecases/authentication/remote_authentication_test.dart
+++ test/data/usecases/authentication/remote_authentication_test.dart
@@ -4,14 +4,14 @@ import 'package:test/test.dart';
 import 'package:meta/meta.dart';
 
 void main() {
-  test('Should call HttpClient with correct URL', () async {
+  test('Should call HttpClient with correct values', () async {
     final httpClient = HttpClientSpy();
     final url = faker.internet.httpUrl();
     final sut = RemoteAuthentication(httpClient: httpClient, url: url);
 
     await sut.auth();
 
-    verify(httpClient.request(url: url));
+    verify(httpClient.request(url: url, method: 'post'));
   });
 }
 
@@ -22,12 +22,12 @@ class RemoteAuthentication {
   RemoteAuthentication({@required this.httpClient, @required this.url});
 
   Future<void> auth() async {
-    await httpClient.request(url: url);
+    await httpClient.request(url: url, method: 'post');
   }
 }
 
 abstract class HttpClient {
-  Future<void> request({@required String url});
+  Future<void> request({@required String url, @required String method});
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
