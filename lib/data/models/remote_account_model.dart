1af4b6ff1bf323f8252f302ff1c6331c40070cd3
kleber.silva <kleber.silva@iteris.com.br>
2020-08-21 01:32:33 -0300

    todos os testes passando

--- lib/data/models/remote_account_model.dart
+++ lib/data/models/remote_account_model.dart
@@ -1,3 +1,4 @@
+import 'package:enquete/data/http/http.dart';
 import 'package:enquete/domain/entities/account_entity.dart';
 
 class RemoteAccountModel {
@@ -5,7 +6,12 @@ class RemoteAccountModel {
 
   RemoteAccountModel(this.accessToken);
 
-  factory RemoteAccountModel.fromJson(Map json) => RemoteAccountModel(json['accessToken']);
+  factory RemoteAccountModel.fromJson(Map json) {
+    if (!json.containsKey('accessToken')) {
+      throw HttpError.invalidData;
+    }
+    return RemoteAccountModel(json['accessToken']);
+  }
 
   AccountEntity toEntity() => AccountEntity(accessToken);
 }
