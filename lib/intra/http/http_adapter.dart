7d9f3104a2774dfddfc3c695dc01aa570f9d5522
kleber.silva <kleber.silva@iteris.com.br>
2020-09-10 22:19:28 -0300

    refactor testes

--- lib/intra/http/http_adapter.dart
+++ lib/intra/http/http_adapter.dart
@@ -2,4 +2,4 @@ import 'dart:html';
 
 import 'package:enquete/data/http/http.dart';
 
-class HttpAdapter {}
+//class HttpAdapter {}
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:enquete/data/http/http.dart';
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
@@ -6,30 +9,85 @@ import 'package:meta/meta.dart';
 
 class ClientSpy extends Mock implements Client {}
 
-class HttpAdapter {
+class HttpAdapter implements HttpClient {
   final Client client;
 
   HttpAdapter(this.client);
-  Future<void> request({
+
+  @override
+  Future<Map> request({
     @required String url,
     @required String method,
     Map body,
     Map headers,
   }) async {
-    await client.post(url);
+    final headers = {
+      'contet-type': 'application/json',
+      'accept': 'application/json',
+    };
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    final response = await client.post(url, headers: headers, body: jsonBody);
+    return response.body.isEmpty ? null : jsonDecode(response.body);
   }
 }
 
 void main() {
+  HttpAdapter sut;
+  ClientSpy client;
+  String url;
+  setUp(() {
+    client = ClientSpy();
+    sut = HttpAdapter(client);
+    url = faker.internet.httpUrl();
+  });
   group('post', () {
+    PostExpectation mockRequest() => when(client.post(any, body: anyNamed('body'), headers: anyNamed('headers')));
+    void mockResponse(int statusCode, {String body = '{"any_key":"any_value"}'}) {
+      mockRequest().thenAnswer(
+        (realInvocation) async => Response(body, statusCode),
+      );
+    }
+
+    setUp(() {
+      mockResponse(200);
+    });
     test('Should call post with correct values', () async {
-      final client = ClientSpy();
-      final sut = HttpAdapter(client);
-      final url = faker.internet.httpUrl();
+      await sut.request(url: url, method: 'post', body: {'any_key': 'any_value'});
 
-      await sut.request(url: url, method: 'post');
+      verify(client.post(url,
+          headers: {
+            'contet-type': 'application/json',
+            'accept': 'application/json',
+          },
+          body: '{"any_key":"any_value"}'));
+    });
+    test('Should call post without body', () async {
+      await sut.request(
+        url: url,
+        method: 'post',
+      );
 
-      verify(client.post(url));
+      verify(client.post(
+        any,
+        headers: anyNamed('headers'),
+      ));
+    });
+    test('Should return data if post returns 200', () async {
+      final response = await sut.request(
+        url: url,
+        method: 'post',
+      );
+
+      expect(response, {'any_key': 'any_value'});
+    });
+    test('Should return null if post returns 200 with no data', () async {
+      mockResponse(200, body: '');
+      final response = await sut.request(
+        url: url,
+        method: 'post',
+      );
+
+      expect(response, null);
     });
   });
 }
